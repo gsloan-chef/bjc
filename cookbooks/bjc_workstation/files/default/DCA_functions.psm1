1cec32e07100f6d1bcd6f7cc62d06b30f7e890af
Nick Rycar <rycar@chef.io>
2018-02-02 10:09:26 -0800

    Adding functions and startup script for CM demo.

--- cookbooks/bjc_workstation/files/default/DCA_functions.psm1
+++ cookbooks/bjc_workstation/files/default/DCA_functions.psm1
@@ -1,11 +1,38 @@
+function DCA-Bootstrap-SSH {
+  param (
+    $name,
+    $env,
+    $cookbook,
+    $recipe
+  )
+  Write-Output "Bootstrapping host $name into $env with recipe $cookbook::$recipe"
+  knife bootstrap $name -N $name -E $env -r "recipe[$cookbook::$recipe]" -i ~/.ssh/id_rsa -x ubuntu --sudo -y
+}
+
+function DCA-Nuke-Automate {
+  Write-Output "TERMINATING..."
+  ssh automate "sudo curl -X DELETE 'http://localhost:9200/_all' && sudo automate-ctl reconfigure"
+}
+
+function DCA-AWS-Create {
+  param (
+    $env,
+    $cookbook,
+    $recipe
+  )
+  Write-Output "Running knife-ec2 to bootstrap node in $env"
+  knife ec2 server create -r "'''recipe[$cookbook::$recipe]'''" -f m4.large -E $env -S chef_demo_2x --image ami-70b67d10 --security-group-ids sg-1cea9178 -T instance-type=DCA-kitchen-ec2 -i ~/.ssh/id_rsa --user-data C:\Users\chef\ubuntu_user_data -x ubuntu --use-iam-profile
+}
+
 function Update-RunLists {
     param (
         $env,
+        $cookbook,
         $recipe
     )
-    Write-Output "Adding the dca_demo::$recipe to nodes in $env"
+    Write-Output "Adding the $cookbook::$recipe to nodes in $env"
     foreach( $node in ` knife node list -E $env ` ) {
-    knife node run_list add $node "'''recipe[dca_demo::$recipe]'''"
+    knife node run_list add $node "'''recipe[$cookbook::$recipe]'''"
     }
 }
 
@@ -20,10 +47,12 @@ function Invoke-ChefClient {
 function DCA-Update-Nodes {
   param(
     $env,
+    $cookbook,
     $recipe
   )
   $environment = $env
-  if ($env -match "dev"){
+  if ($env -match "awsdev"){
+  } elseif ($env -match "dev"){
     $environment = "development"
   } elseif ($env -match "sta"){
     $environment = "staging"
@@ -31,9 +60,9 @@ function DCA-Update-Nodes {
     $environment = "production"
   }
 
-  Write-Output "Updating nodes in $environment with dca_demo::$recipe."
+  Write-Output "Updating nodes in $environment with $cookbook::$recipe."
   foreach( $node in ` knife node list -E $environment ` ) {
-    knife node run_list add $node "'''recipe[dca_demo::$recipe]'''"
+    knife node run_list add $node "'''recipe[$cookbook::$recipe]'''"
   }
   Write-Output "Converging nodes in the $environment environment"
   knife ssh "chef_environment:$environment" 'sudo chef-client'
---
+++ cookbooks/bjc_workstation/files/default/Start_CM.ps1
@@ -0,0 +1,69 @@
+Write-Host -ForegroundColor Green "[1/9] Logging into automate with inspec"
+inspec compliance login_automate https://automate.automate-demo.com --insecure --user='workstation-1' --ent='automate-demo' --dctoken='93a49a4f2482c64126f7b6015e6b0f30284287ee4054ff8807fb63d9cbd1c506'
+
+Write-Host -ForegroundColor Green "[2/9] Uploading Planex Validation Profile"
+cd C:\Users\chef\dca\
+inspec archive C:\Users\chef\profiles\planex_validate
+inspec compliance upload C:\Users\chef\dca\planex_validate-0.1.0.tar.gz
+
+Write-Host -ForegroundColor Green "[3/9] Installing bjc-ecommerce on build nodes"
+
+Workflow buildnode-update {
+  foreach -parallel ($node in @("build-node-1","build-node-2","build-node-3")) {
+    ssh $node "sudo chef-client -o '''recipe[bjc-ecommerce::tksetup],recipe[bjc-ecommerce]'''"
+  }
+}
+
+cd ~
+buildnode-update
+
+Write-Host -ForegroundColor Green "[4/9] Assimilating Hostsfile"
+
+$hostsfile = "C:\Windows\System32\drivers\etc\hosts"
+
+"172.31.54.101" + "`t`t" + "dev1" | Out-File -encoding ASCII -append $hostsfile
+"172.31.54.102" + "`t`t" + "dev2" | Out-File -encoding ASCII -append $hostsfile
+"172.31.54.103" + "`t`t" + "stage1" | Out-File -encoding ASCII -append $hostsfile
+"172.31.54.104" + "`t`t" + "stage2" | Out-File -encoding ASCII -append $hostsfile
+"172.31.54.51" + "`t`t" + "prod1" | Out-File -encoding ASCII -append $hostsfile
+"172.31.54.52" + "`t`t" + "prod2" | Out-File -encoding ASCII -append $hostsfile
+"172.31.54.53" + "`t`t" + "prod3" | Out-File -encoding ASCII -append $hostsfile
+
+Write-Host -ForegroundColor Green "[5/9] Updating Automate Hostsfile"
+
+ssh automate 'echo "172.31.54.101  dev1" | sudo tee --append /etc/hosts'
+ssh automate 'echo "172.31.54.102  dev2"  | sudo tee --append /etc/hosts'
+ssh automate 'echo "172.31.54.103  stage1" | sudo tee --append /etc/hosts'
+ssh automate 'echo "172.31.54.104  stage2" | sudo tee --append /etc/hosts'
+ssh automate 'echo "172.31.54.51  prod1" | sudo tee --append /etc/hosts'
+ssh automate 'echo "172.31.54.52  prod2" | sudo tee --append /etc/hosts'
+ssh automate 'echo "172.31.54.53  prod3" | sudo tee --append /etc/hosts'
+
+
+Write-Host -ForegroundColor Green "[6/9] Hardening Infra Nodes"
+
+Workflow infra-harden {
+  foreach -parallel ($node in @("dev1", "dev2", "stage1", "stage2", "prod1", "prod2", "prod3") ) {
+    ssh $node "sudo chef-client -o '''recipe[dca_demo::hardening]'''"
+  }
+}
+
+cd ~
+infra-harden
+
+Write-Host -ForegroundColor Green "[7/9] Nuking Automate Data"
+
+ssh automate "sudo curl -X DELETE 'http://localhost:9200/_all' && sudo automate-ctl reconfigure"
+
+Write-Host -ForegroundColor Green "[8/9] Updating path and loading custom functions."
+$env:path = "C:\Users\chef\dca;$env:path"
+Import-Module DCA_functions
+
+Write-Host -ForegroundColor Green "[9/10] Updating knife config with aws region"
+"knife[:region] = 'us-west-2'" | Out-File -encoding ASCII -append C:\Users\chef\.chef\knife.rb
+
+
+Write-Host -ForegroundColor Green "[9/9] Opening Chrome Tabs & Cmder"
+start-process "chrome.exe" "https://automate.automate-demo.com/", '--profile-directory="Default"'
+start-process "chrome.exe" "https://prod1/cart",'--profile-directory="Default"'
+start-process "chrome.exe" "https://dev2/cart",'--profile-directory="Default"'
