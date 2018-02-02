d1786d971304dd1fa9d88e0a19ccef67c7b5abf8
Nick Rycar <rycar@chef.io>
2018-02-02 09:41:20 -0800

    Adding all DCA cookbooks to windows workstation.

--- cookbooks/bjc_workstation/attributes/default.rb
+++ cookbooks/bjc_workstation/attributes/default.rb
@@ -1,2 +1,2 @@
-default['bjc_workstation']['cookbooks'] = ['bjc-ecommerce', 'bjc_bass', 'dca_demo']
+default['bjc_workstation']['cookbooks'] = ['bjc-ecommerce', 'bjc_bass', 'dca_demo', 'dca_audit_baseline', 'dca_hardening_linux']
 default['bjc_workstation']['profiles'] = %w(apache_webserver linux_baseline_wrapper windows_baseline_wrapper)
