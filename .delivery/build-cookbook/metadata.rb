ba1748b93dd7889f6cd28c80de4f5b5b6e19ec7b
Nick Rycar <rycar@chef.io>
2018-03-08 15:44:53 -0800

    Removing more conditional builds. Always build. Always.

--- .delivery/build-cookbook/metadata.rb
+++ .delivery/build-cookbook/metadata.rb
@@ -2,7 +2,7 @@ name 'build-cookbook'
 maintainer 'The Authors'
 maintainer_email 'you@example.com'
 license 'all_rights'
-version '0.3.3'
+version '0.3.4'
 
 depends 'sbp_packer'
 depends 'delivery-truck'
--- .delivery/build-cookbook/recipes/publish.rb
+++ .delivery/build-cookbook/recipes/publish.rb
@@ -23,56 +23,53 @@ execute 'copy-packer-keys' do
   action :run
 end
 
-# Only build if there are changed cookbooks
-unless changed_cookbooks.empty?
-  # Build new Chef Demo AMIs
+# Build new Chef Demo AMIs
 
-  # we're picking ubuntu runners for AWS builds
-  cloud = case node['platform']
-          when 'ubuntu'
-            'aws'
-          else
-            'azure'
-          end
+# we're picking ubuntu runners for AWS builds
+cloud = case node['platform']
+        when 'ubuntu'
+          'aws'
+        else
+          'azure'
+        end
 
-  %w[build update deploy].each do |s|
-    template "#{workflow_workspace}/wombat_#{s}.sh" do
-      source "wombat_#{s}.sh.erb"
-      mode '0755'
-      variables(cloud: cloud,
-                owca_fqdn: node['owca']['fqdn'])
-      action :create
-    end
+%w[build update deploy].each do |s|
+  template "#{workflow_workspace}/wombat_#{s}.sh" do
+    source "wombat_#{s}.sh.erb"
+    mode '0755'
+    variables(cloud: cloud,
+              owca_fqdn: node['owca']['fqdn'])
+    action :create
   end
+end
 
-  execute 'build-the-things' do
-    command "#{workflow_workspace}/wombat_build.sh"
-    live_stream true
-    cwd workspace
-    action :run
-  end
+execute 'build-the-things' do
+  command "#{workflow_workspace}/wombat_build.sh"
+  live_stream true
+  cwd workspace
+  action :run
+end
 
-  # Create a new bjc-demo.json template
-  execute 'generate-json' do
-    command "#{workflow_workspace}/wombat_update.sh"
-    live_stream false
-    cwd workspace
-    action :run
-  end
+# Create a new bjc-demo.json template
+execute 'generate-json' do
+  command "#{workflow_workspace}/wombat_update.sh"
+  live_stream false
+  cwd workspace
+  action :run
+end
 
-  # Use the Ruby AWS SDK to upload bjc-demo.json to S3
+# Use the Ruby AWS SDK to upload bjc-demo.json to S3
 
-  # Use ruby in the compile phase for testing
-  # s3 = Aws::S3::Resource.new(region:'us-west-2')
-  # obj = s3.bucket('bjcpublic').object('bjc-demo.json')
-  # obj.upload_file("/var/opt/delivery/workspace/#{node['owca']['fqdn']}/default/chef-sas/bjc/master/build/publish/repo/stacks/bjc-demo.json", acl:'public-read')
+# Use ruby in the compile phase for testing
+# s3 = Aws::S3::Resource.new(region:'us-west-2')
+# obj = s3.bucket('bjcpublic').object('bjc-demo.json')
+# obj.upload_file("/var/opt/delivery/workspace/#{node['owca']['fqdn']}/default/chef-sas/bjc/master/build/publish/repo/stacks/bjc-demo.json", acl:'public-read')
 
-  # And a ruby_block for executing *after* the builds are done
-  ruby_block "upload bjc-demo-#{cloud}.json to S3" do
-    block do
-      s3 = Aws::S3::Resource.new(region: 'us-west-2')
-      obj = s3.bucket('bjcpublic').object("acceptance-bjc-demo-#{cloud}.json")
-      obj.upload_file("#{workflow_workspace}/#{node['owca']['fqdn']}/default/chef-sas/bjc/master/build/publish/repo/stacks/bjc-demo.json", acl: 'public-read')
-    end
+# And a ruby_block for executing *after* the builds are done
+ruby_block "upload bjc-demo-#{cloud}.json to S3" do
+  block do
+    s3 = Aws::S3::Resource.new(region: 'us-west-2')
+    obj = s3.bucket('bjcpublic').object("acceptance-bjc-demo-#{cloud}.json")
+    obj.upload_file("#{workflow_workspace}/#{node['owca']['fqdn']}/default/chef-sas/bjc/master/build/publish/repo/stacks/bjc-demo.json", acl: 'public-read')
   end
 end
