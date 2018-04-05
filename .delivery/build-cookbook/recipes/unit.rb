870b6b40dc82f27187ff7a524401d53272bfed8a
Nick Rycar <rycar@chef.io>
2018-04-05 13:04:25 -0700

    Oh right, YAML.load is fine for the second case.

--- .delivery/build-cookbook/recipes/unit.rb
+++ .delivery/build-cookbook/recipes/unit.rb
@@ -8,7 +8,7 @@ raise 'You must update CHANGELOG.md before your change can be promoted!' if !cha
 
 change_demo_ver = YAML.load_file("#{workflow_workspace_repo}/wombat.yml")['version']
 master_wombat_yml = shell_out!('git show master:wombat.yml', cwd: workflow_workspace_repo).stdout
-master_demo_ver = YAML.load_file(master_wombat_yml)['version']
+master_demo_ver = YAML.load(master_wombat_yml)['version']
 
 raise 'You must update the demo version in wombat.yml before your change can be promoted!' if(change_demo_ver == master_demo_ver)
 
