fef5d3c3e7fdb2acce7a12750ff9c9465fde911d
Nick Rycar <rycar@chef.io>
2018-04-05 12:57:11 -0700

    Colons, not slashes

--- .delivery/build-cookbook/recipes/unit.rb
+++ .delivery/build-cookbook/recipes/unit.rb
@@ -7,7 +7,7 @@
 raise 'You must update CHANGELOG.md before your change can be promoted!' if !changed_files.include?("CHANGELOG.md")
 
 change_demo_ver = YAML.load_file("#{workflow_workspace_repo}/wombat.yml")['version']
-master_wombat_yml = shell_out!('git show master/wombat.yml', cwd: workflow_workspace_repo)
+master_wombat_yml = shell_out!('git show master:wombat.yml', cwd: workflow_workspace_repo)
 master_demo_ver = YAML.load_file(master_wombat_yml)['version']
 
 raise 'You must update the demo version in wombat.yml before your change can be promoted!' if(change_demo_ver == master_demo_ver)
