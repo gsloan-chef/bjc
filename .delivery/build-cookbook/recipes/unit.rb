394cacecc44dcea47bb4e0eccbe2ba6376f297af
Nick Rycar <rycar@chef.io>
2018-04-05 13:13:34 -0700

    Make sure we evaluate origin's master, not local. Fixes #676

--- .delivery/build-cookbook/recipes/unit.rb
+++ .delivery/build-cookbook/recipes/unit.rb
@@ -7,7 +7,7 @@
 raise 'You must update CHANGELOG.md before your change can be promoted!' if !changed_files.include?("CHANGELOG.md")
 
 change_demo_ver = YAML.load_file("#{workflow_workspace_repo}/wombat.yml")['version']
-master_wombat_yml = shell_out!('git show master:wombat.yml', cwd: workflow_workspace_repo).stdout
+master_wombat_yml = shell_out!('git show origin/master:wombat.yml', cwd: workflow_workspace_repo).stdout
 master_demo_ver = YAML.load(master_wombat_yml)['version']
 
 raise 'You must update the demo version in wombat.yml before your change can be promoted!' if(change_demo_ver == master_demo_ver)
