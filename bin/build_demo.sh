a9fc0901a0add75e93df18f5b2c35e630a876819
Jeff Mery <jeff@merys.net>
2018-04-26 11:31:45 -0500

    Add X-Application required tag to start script

--- bin/build_demo.sh
+++ bin/build_demo.sh
@@ -2,7 +2,7 @@
 # Builds Chef Demo environment with a Cloudformation template
 
 # Usage and help
-USAGE="Usage: $0 [demo-version] [customer name or description] [your aws key name] [hours to keep the demo up] [your name] [your department]\n\nExample:\n$0 1.2.0 'Paper Street' \$AWS_KEYPAIR_NAME 4 'Tyler Durden' 'Sales'"
+USAGE="Usage: $0 [demo-version] [customer name or description] [your aws key name] [hours to keep the demo up] [your name] [your department] [your region]\n\nExample:\n$0 1.2.0 'Paper Street' \$AWS_KEYPAIR_NAME 4 'Tyler Durden' 'Sales' 'NA-East'"
 
 if [[ $# -ne 6 ]]; then
   echo -e $USAGE
@@ -17,6 +17,7 @@ SSH_KEY=$3
 TTL=$4
 CONTACT=$5
 DEPARTMENT=$6
+REGION=$7
 TERMINATION_DATE="$(TZ=Etc/UTC date -j -v +$4H +'%Y-%m-%dT%H:%M:%SZ')"
 REGION=us-west-2
 
@@ -25,6 +26,6 @@ aws cloudformation create-stack \
 --stack-name "${USER}-${CUSTOMER}-Chef-Demo-$(TZ=Etc/UTC date +'%Y%m%dT%H%M%SZ')" \
 --capabilities CAPABILITY_IAM \
 --region $REGION \
---tags Key=X-TTL,Value=${TTL} Key=TTL,Value=${TTL} Key=X-Contact,Value="${CONTACT}" Key=X-Dept,Value="${DEPARTMENT}" Key=X-Customer,Value="${CUSTOMER}" Key=X-Project,Value="BJC-Demo" Key=X-Termination-Date,Value=${TERMINATION_DATE} \
+--tags Key=X-TTL,Value=${TTL} Key=TTL,Value=${TTL} Key=X-Contact,Value="${CONTACT}" Key=X-Dept,Value="${DEPARTMENT}" Key=X-Customer,Value="${CUSTOMER}" Key=X-Project,Value="BJC-Demo" Key=X-Termination-Date,Value=${TERMINATION_DATE} Key=X-Application,Value="${REGION}"\
 --template-url https://s3-us-west-2.amazonaws.com/bjcpublic/cloudformation/bjc-demo-${VERSION}.json \
 --parameters ParameterKey=KeyName,ParameterValue=${SSH_KEY} ParameterKey=TTL,ParameterValue=${TTL}
