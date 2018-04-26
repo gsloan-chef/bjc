69d5959b2f347d2b0b898ed1c2664150b487e726
Jeff Mery <jeff@merys.net>
2018-04-26 11:38:49 -0500

    Fix arg number; space behind command

--- bin/build_demo.sh
+++ bin/build_demo.sh
@@ -4,7 +4,7 @@
 # Usage and help
 USAGE="Usage: $0 [demo-version] [customer name or description] [your aws key name] [hours to keep the demo up] [your name] [your department] [your region]\n\nExample:\n$0 1.2.0 'Paper Street' \$AWS_KEYPAIR_NAME 4 'Tyler Durden' 'Sales' 'NA-East'"
 
-if [[ $# -ne 6 ]]; then
+if [[ $# -ne 7 ]]; then
   echo -e $USAGE
   exit 1
 fi
@@ -26,6 +26,6 @@ aws cloudformation create-stack \
 --stack-name "${USER}-${CUSTOMER}-Chef-Demo-$(TZ=Etc/UTC date +'%Y%m%dT%H%M%SZ')" \
 --capabilities CAPABILITY_IAM \
 --region $REGION \
---tags Key=X-TTL,Value=${TTL} Key=TTL,Value=${TTL} Key=X-Contact,Value="${CONTACT}" Key=X-Dept,Value="${DEPARTMENT}" Key=X-Customer,Value="${CUSTOMER}" Key=X-Project,Value="BJC-Demo" Key=X-Termination-Date,Value=${TERMINATION_DATE} Key=X-Application,Value="${REGION}"\
+--tags Key=X-TTL,Value=${TTL} Key=TTL,Value=${TTL} Key=X-Contact,Value="${CONTACT}" Key=X-Dept,Value="${DEPARTMENT}" Key=X-Customer,Value="${CUSTOMER}" Key=X-Project,Value="BJC-Demo" Key=X-Termination-Date,Value=${TERMINATION_DATE} Key=X-Application,Value="${REGION}" \
 --template-url https://s3-us-west-2.amazonaws.com/bjcpublic/cloudformation/bjc-demo-${VERSION}.json \
 --parameters ParameterKey=KeyName,ParameterValue=${SSH_KEY} ParameterKey=TTL,ParameterValue=${TTL}
