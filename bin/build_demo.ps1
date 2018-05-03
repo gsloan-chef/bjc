a154740ec3e8d419e6ac196f6bc0b731545cd6ca
Jeff Mery <jeff@merys.net>
2018-05-03 06:25:13 -0500

    Add cloud platform support to powershell script launcher

--- bin/build_demo.ps1
+++ bin/build_demo.ps1
@@ -4,16 +4,19 @@
 Deploys a Chef Demo environment on a target cloud platform.
 
 .EXAMPLE
-./build_demo.ps1 [demo-version] [customer name or description] [your aws key name] [hours to keep the demo up] [your name] [your department] [your region]
+./build_demo.ps1 [cloud platform] [demo-version] [customer name or description] [your aws key name] [hours to keep the demo up] [your name] [your department] [your region]
 
-Deploy latest stable version (defaults to AWS)
-./build_demo.ps1 stable 'Paper Street' my_key_pair 4 'Tyler Durden' 'Sales' 'NA-East'
+Deploy latest stable version on AWS
+./build_demo.ps1 aws stable 'Paper Street' my_key_pair 4 'Tyler Durden' 'Sales' 'NA-East'
+
+Deploy latest stable version on Azure
+./build_demo.ps1 azure stable 'Paper Street' my_key_pair 4 'Tyler Durden' 'Sales' 'NA-East'
 
 Depoly a specific version on AWS
-./build_demo.ps1 aws-4.5.4 'Paper Street' my_key_pair 4 'Tyler Durden' 'Sales' 'NA-East'
+./build_demo.ps1 aws 4.5.4 'Paper Street' my_key_pair 4 'Tyler Durden' 'Sales' 'NA-East'
 
 Deploy a specific version on Azure
-./build_demo.ps1 azure-4.5.4 'Paper Street' my_key_pair 4 'Tyler Durden' 'Sales' 'NA-East'
+./build_demo.ps1 azure 4.5.4 'Paper Street' my_key_pair 4 'Tyler Durden' 'Sales' 'NA-East'
 
 .LINK
 https://github.com/chef-cft/bjc
@@ -23,24 +26,27 @@ https://github.com/chef-cft/bjc
 [CmdletBinding()]
 Param(
   [Parameter(Mandatory=$True,Position=1)]
+    [string]$cloud,
+
+  [Parameter(Mandatory=$True,Position=2)]
     [string]$version,
-  
-    [Parameter(Mandatory=$True,Position=2)]
+
+  [Parameter(Mandatory=$True,Position=3)]
     [string]$customer,
-  
-    [Parameter(Mandatory=$True,Position=3)]
+
+  [Parameter(Mandatory=$True,Position=4)]
     [string]$ssh_key,
-  
-    [Parameter(Mandatory=$True,Position=4)]
+
+  [Parameter(Mandatory=$True,Position=5)]
     [string]$ttl,
-  
-    [Parameter(Mandatory=$True,Position=5)]
+
+  [Parameter(Mandatory=$True,Position=6)]
     [string]$contact,
-  
-    [Parameter(Mandatory=$True,Position=6)]
+
+  [Parameter(Mandatory=$True,Position=7)]
     [string]$department,
-  
-    [Parameter(Mandatory=$True,Position=7)]
+
+  [Parameter(Mandatory=$True,Position=8)]
     [string]$location
 )
 
@@ -57,4 +63,22 @@ $utc_date = Get-Date -date $utc_date -format "yyyyMMddTHHmmssZ"
 $termination_date = Get-Date -date $termination_date -format "yyyy-MM-ddTHH:mm:ssZ"
 
 # Here's where we create the stack
-aws cloudformation create-stack --stack-name "$env:USERNAME-$customer-Chef-Demo-$utc_date" --capabilities CAPABILITY_IAM --region $region --tags Key=X-TTL,Value=$ttl Key=TTL,Value=$ttl Key=X-Contact,Value="$contact" Key=X-Dept,Value="$department" Key=X-Customer,Value="$customer" Key=X-Project,Value="BJC-Demo" Key=X-Termination-Date,Value=$termination_date Key=X-Application,Value=$location --template-url https://s3-us-west-2.amazonaws.com/bjcpublic/cloudformation/bjc-demo-$version.json --parameters ParameterKey=KeyName,ParameterValue=$ssh_key ParameterKey=TTL,ParameterValue=$ttl
+
+
+switch ($cloud)
+{
+  'aws'
+  {
+    write-host "Creating $cloud $version demo..."
+    aws cloudformation create-stack --stack-name "$env:USERNAME-$customer-Chef-Demo-$utc_date" --capabilities CAPABILITY_IAM --region $region --tags Key=X-TTL,Value=$ttl Key=TTL,Value=$ttl Key=X-Contact,Value="$contact" Key=X-Dept,Value="$department" Key=X-Customer,Value="$customer" Key=X-Project,Value="BJC-Demo" Key=X-Termination-Date,Value=$termination_date Key=X-Application,Value=$location --template-url https://s3-us-west-2.amazonaws.com/bjcpublic/cloudformation/bjc-demo-$cloud-$version.json --parameters ParameterKey=KeyName,ParameterValue=$ssh_key ParameterKey=TTL,ParameterValue=$ttl
+  }
+  'azure'
+  {
+    write-host "$cloud platorm support in this script is coming coon!"
+  }
+  default
+  {
+    write-host "$cloud cloud platform is not currently supported"
+    write-host "Please submit an issue to https://github/chef-cft/bjc to request a new platform."
+  }
+}
